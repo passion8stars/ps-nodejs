a07dc922a3c9363c86fe92adb741cab15465dd6a
PS <mail2parvind@gmail.com>
2019-12-08 00:04:47 +0530

    404 route added

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Passionate Stars</h1>')
+})
+app.get('*', (req, res) => {
+    res.send('<h1>Welcome to Passionate Stars</h1><p>Sorry, requested page didn\'t found.')
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
